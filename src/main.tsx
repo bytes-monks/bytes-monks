6b9012f2ce6bc3700ec1f5b1bb8402a3fb580753
somebodyawesome-dev <luckynoob2011830@gmail.com>
2026-03-28 15:17:17 +0100

    fix: fix scrolling issue when switching pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
 import App from './App'
 import TermsOfService from './pages/TermsOfService'
 import PrivacyPolicy from './pages/PrivacyPolicy'
@@ -9,9 +9,18 @@ import Pricing from './pages/Pricing'
 import Hiring from './pages/Hiring'
 import './index.css'
 
+function ScrollToTop() {
+  const { pathname } = useLocation()
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+  return null
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/pricing" element={<Pricing />} />
